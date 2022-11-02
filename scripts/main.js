9c18c39e9f62d3a9c7f852e17dfab8d9d0d513c3
Ethan Straub <evstraub@gmail.com>
2022-11-02 09:02:14 -0400

    added basic files

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+let myLibrary = [
+  "Fire & Blood",
+  "A Game of Thrones",
+  "A Clash of Kings",
+  "A Storm of Swords",
+  "A Dance With Dragons",
+];
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+function addBookToLibrary() {
+  // do stuff here
+}
