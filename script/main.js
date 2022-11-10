ab4b79652a158efcd10deb7dc684f7986cfba942
Ethan Straub <evstraub@gmail.com>
2022-11-09 20:39:01 -0500

    keep forgrtting to commit and push, but added input form and connected js.

--- script/main.js
+++ script/main.js
@@ -29,23 +29,47 @@ function displayBooksOnPage() {
       "<br>" +
       "Read?: " +
       item.read +
+      "<br>" +
+      "<button>delete</button>" +
       "</div>"
     );
   });
   info.innerHTML = details.join("\n");
 }
+const deleteBtn = document.querySelector("delete");
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
 
-addBookToLibrary("pepe", "poopo", "2000", "yes");
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+const submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", intakeFormData);
+function intakeFormData() {
+  let title = document.getElementById("Title").value;
+  let author = document.getElementById("Author").value;
+  let pages = document.getElementById("Pages").value;
+  let read = document.getElementById("Read").value;
+
+  // Break out if form is incomplete or not valid
+  if (title == "" || author == "" || pages == "" || read == "") {
+    return;
+  }
+
+  // Call function to input the book data to array
+  addBookToLibrary(title, author, pages, read);
+
+  // Reset the form after successful submission
+  document.getElementById("form2").reset();
+}
+
+addBookToLibrary("The Glass Hotel", "Emily Stjn Mandell", "301", "Yes");
 addBookToLibrary("Blackthongue Thief", "Chris Buelhman", "412", "yes");
 addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
-addBookToLibrary("It Ends With Us", "Colleen Hoover", "315", "yes");
 
-console.log(myLibrary);
+//console.log(myLibrary);
